899a7780596cfeed4be4f002025f204090e8e268
emilyboy <emilycarvalhoboy086@gmail.com>
2026-08-18 16:11:38 -0300

    Assunto2
    inclusão de material

---
+++ Assunto 2 - Protocolo HTTP/1_cadastro_status.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro - Status Codes</title>
+</head>
+<body>
+
+<h1>Cadastro de Aluno (com status Code)</h1>
+<form action="" method="post" action= "">
+    <label for="nome">Nome:</label>
+    <input type="text" name="nome" required><br><br>
+
+     <label for="idade">Idade:</label>
+     <input type="text" name="idade" required><br><br>
+   
+    <button type="submit">Enviar</button>
+
+</form>
+
+</body>
+</html>
